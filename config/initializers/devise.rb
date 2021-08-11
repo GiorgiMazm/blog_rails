ccdf44e62ac415bd1c515296a8c94162fbab2895
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2021-08-11 14:24:33 +0200

    fiexed log in and edit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -169,6 +169,8 @@ Devise.setup do |config|
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
 
+  config.scoped_views = true
+
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
 
