33bbdd79a77b2391febce2d92827426d655afd4a
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2021-08-12 10:27:22 +0200

    finished login fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_112735) do
+ActiveRecord::Schema.define(version: 2021_08_11_130915) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2021_08_11_112735) do
     t.string "creator"
     t.string "writer"
     t.date "release"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
