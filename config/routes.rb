456b61b694e261badc1ff4c734715faf6b848efd
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2021-08-16 10:54:16 +0200

    add all users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   resources :comments
   root to: 'homepage#index'
   resources :posts
+  match '/users',   to: 'users#index',   via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
