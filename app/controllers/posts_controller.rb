9cc58887431761214afb6baea5e3e3bcafdfc421
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2021-08-17 09:11:11 +0200

    image add fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,6 +67,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :series, :season, :duration, :creator, :writer, :release)
+      params.require(:post).permit(:title, :body, :series, :season, :duration, :creator, :writer, :release, images:[])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     belongs_to :author, class_name: "User"
+    has_many_attached :images
 end
